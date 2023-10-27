2a345f1bad75f28563a8da2ec4aab129fd31c333
robreijtenbach <rob@reijtenbach.com>
2023-10-27 16:42:13 +0200

    Implemented differential encoding, get nice compression ratio on l_orderkey files as these are ascending.

--- differential_encoding.py
+++ differential_encoding.py
@@ -1,9 +1,25 @@
 class DifferentialEncoder():
     def encode(self, data, dtype):
         '''Receives data as list of input items, returns encoded data string'''
-        raise NotImplementedError
+        # Check if dtype is integer as only these are needed for this encoding
+        if dtype[:3] != "int": 
+            raise TypeError
+        encodedData = "{}\n".format(data[0])
+        for i in range(1, len(data)):
+            difference = int(data[i]) - int(data[i-1])
+            encodedData += "{}\n".format(difference)
+        return encodedData
 
     def decode(self, data, dtype):
         '''Receives data as encoded string as created by encode function. 
         Returns list of items that are the same as input of encode function.'''
-        raise NotImplementedError
+        if dtype[:3] != "int": 
+            raise TypeError
+        data = data.split()
+        previousVal = int(data[0])
+        decodedData = [previousVal]
+        for i in range(1, len(data)):
+            currentVal = previousVal + int(data[i])
+            decodedData.append(currentVal)
+            previousVal = currentVal
+        return decodedData
